98514e458bf10568075e990286563b15cda95471
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-18 20:37:46 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.get('/ui/madi.png', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.send(path.join(__dirname,'ui','article-one'));
+  res.sendFile(path.join(__dirname,'ui','article-one.html'));
 });
 app.get('/article-two', function (req, res) {
   res.sendFile(path.join(__dirname,'ui','article-two.html'));
