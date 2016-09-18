9addd384b60706bf244817b4c3658a51c71f4340
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-18 17:39:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ app.get('/ui/style.css', function (req, res) {
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
+
 app.get('/ui/background.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'background.png'));
 });
