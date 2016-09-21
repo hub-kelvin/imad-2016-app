1a384ed20dd4c222f76dc2a4e436081f75214a44
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-21 11:05:04 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
 console.log('Loaded!');
 var element = document.getElementById('logo');
 element.onclick = function () {
-    setIntervel(moveright,50);
+    setInterval(moveright,50);
     };
     var value=0;
 function moveright(){
     
-    element.style.marginLeft=value+'px';
+    element.style.marginLeft=value + 'px';
     value=value+10;
 }    
