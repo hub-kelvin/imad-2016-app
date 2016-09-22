963eb2f57a930cf89906dce33f2bcdbfb3054480
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-22 22:17:23 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -41,6 +41,12 @@ function createTemp(data) {
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+var count=0;
+app.get('/test', function (req, res) {
+  
+  count=count+1;
+  res.send(count.toString());
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
