9c482884eb6b2e9e2812b691675633cf955bf86b
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-22 22:11:28 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var counter=document.getElementById("count");
+var c=0;
+counter.onclick= function(){
+    c=c+1;
+    var temp=document.getElementById("value");
+    temp.innerHTML=(c.toString());
+}
