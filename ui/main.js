07537f65847c913cd351d5a6a187f83c7a8cb5ea
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-23 22:01:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -19,7 +19,6 @@ counter.onclick= function(){
 
 var input_btn=document.getElementById("input_button");
 input_btn.onclick = function(){
-    alert("ty");
+  var nl= document.getElementById("namelist");
+  nl.innerHTML+='<li>' + document.getElementById("inputname").value + '</li>';
 };
-  // var nl= document.getElementById("namelist");
-  // nl.innerHTML+='<li>' + document.getElementById("inputname").value + '</li>';
