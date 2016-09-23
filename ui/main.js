978781a9b983e326ef01b76013efc2f32cdd3136
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-23 08:16:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,7 @@
 console.log('Loaded!');
 var counter=document.getElementById("count");
 var c=0;
-count.onclick= function(){
+counter.onclick= function(){
     
     var req = new XMLHttpRequest();
     req.onreadystatechange = function()
@@ -9,7 +9,7 @@ count.onclick= function(){
             if(req.readyState===XMLHttpRequest.DONE)
             {
             if(req.status===200)
-            document.getElementById("value").innerhtml=(req.responseText).toString();
+            document.getElementById("value").innerHTML=(req.responseText).toString();
             }
     };
             req.open('GET',"http://hub-kelvin.imad.hasura-app.io/test",true);
