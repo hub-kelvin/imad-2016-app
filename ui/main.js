a01833fecef97313f2bd71a6b40ee03963aaaea1
Kelvin Jose <kelvinkonoor@gmail.com>
2016-09-24 08:30:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,23 +2,26 @@ console.log('Loaded!');
 var counter=document.getElementById("count");
 var c=0;
 var Name="";
-counter.onclick= function(){
+var input_btn=document.getElementById("input_button");
+input_btn.onclick = function(){
     
     var req = new XMLHttpRequest();
     req.onreadystatechange = function()
     {
             if(req.readyState===4)
             {
-            if(req.status===200)
-            document.getElementById("value").innerHTML=(req.responseText).toString();
+            if(req.status===200){
+            
+            var nl= document.getElementById("namelist");
+            nl.innerHTML+='<li>' + req.responseText + '</li>';
+                
+            }
+                
             }
     };
-            req.open('GET',"http://hub-kelvin.imad.hasura-app.io/test",true);
+            req.open('GET',"http://hub-kelvin.imad.hasura-app.io/submit?=" + document.getElementById("inputname").value ,true);
             req.send(null);
-};
 
-var input_btn=document.getElementById("input_button");
-input_btn.onclick = function(){
-  var nl= document.getElementById("namelist");
-  nl.innerHTML+='<li>' + document.getElementById("inputname").value + '</li>';
+
+
 };
