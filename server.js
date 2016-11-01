76d05902df6c64c55d421985966ab17b85570629
Kelvin Jose <kelvinkonoor@gmail.com>
2016-11-01 07:55:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -89,6 +89,9 @@ app.get('/ui/stylesheet.css', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/ui/custom.css', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'custom.css'));
+});
 app.get('/ui/bitstory.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'bitstory.png'));
 });
