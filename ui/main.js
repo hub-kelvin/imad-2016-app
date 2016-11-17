b84ea7a36232476380c5faa0267daa9ba25d53c5
Kelvin Jose <kelvinkonoor@gmail.com>
2016-11-17 21:27:10 +0530

    Update main.js

--- ui/main.js
+++ ui/main.js
@@ -1,30 +1,4 @@
-console.log('Loaded!');
-var counter=document.getElementById("count");
-var c=0;
-var Name="";
-var input_btn=document.getElementById("input_button");
-input_btn.onclick = function(){
-    
-    var req = new XMLHttpRequest();
-    req.onreadystatechange = function()
-    {
-            if(req.readyState===4)
-            {
-            if(req.status===200){
-            
-            var nl= document.getElementById("namelist");
-            nl.innerHTML+='<li>' + req.responseText + '</li>';
-                
-            }
-                
-            }
-    };
-            req.open('GET',"http://hub-kelvin.imad.hasura-app.io/submit?Query=" + document.getElementById("inputname").value ,true);
-            req.send(null);
 
-
-
-};
 function loadLoginForm () {
     
     var loginHtml = `
