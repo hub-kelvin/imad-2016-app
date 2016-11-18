8851a687ec625c98c6595d234f482788e81d1236
Kelvin Jose <kelvinkonoor@gmail.com>
2016-11-18 19:52:01 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -34,7 +34,7 @@ function loadLoginForm () {
     `;
  
     document.getElementById('login_area').innerHTML = loginHtml;
-    
+     document.getElementById("loginform").style.display = 'none';
     // Submit username/password to login
     var submit = document.getElementById('login_btn');
     submit.onclick = function () {
