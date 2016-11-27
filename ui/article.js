e5c4ff4b7b2bec2b6e9df86607fff2b7fff835c9
Kelvin Jose <kelvinkonoor@gmail.com>
2016-11-27 12:03:01 +0530

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -33,7 +33,8 @@ function loadCommentForm () {
         
         // Make the request
         var comment = document.getElementById('comment_text').value;
-        if (comment !== '') {
+        
+        if(!isEmpty(comment)) {
         request.open('POST', '/submit-comment/' + currentArticleTitle, true);
         request.setRequestHeader('Content-Type', 'application/json');
         request.send(JSON.stringify({comment: comment}));  
